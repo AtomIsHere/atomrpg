e493f1cb52ee81fdb6b69b417d137b06670a90b9
AtomIsHere <archietomoconnor@gmail.com>
2023-02-15 15:11:17 +1100

    Re-implement shadow and re-add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     `java-library`
 
     id("io.papermc.paperweight.userdev") version "1.5.1-LOCAL-SNAPSHOT"
+
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "com.github.atomishere"
@@ -12,8 +14,19 @@ java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.aikar.co/content/groups/aikar/") }
+}
+
 dependencies {
     paperDevBundle("1.19.3-R0.1-SNAPSHOT")
+
+    implementation("com.google.inject:guice:5.1.0")
+    implementation("io.github.classgraph:classgraph:4.8.154")
+    implementation("io.github.toolfactory:narcissus:1.0.7")
+
+    implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
 }
 
 tasks {
@@ -31,5 +44,12 @@ tasks {
     }
     processResources {
         filteringCharset = Charsets.UTF_8.name()
+    }
+
+    shadowJar {
+        fun reloc(pkg: String) = relocate(pkg, "com.github.atomishere.atomrpg.depends")
+
+        reloc("co.aikar.commands")
+        reloc("co.aikar.locales")
     }
 }
